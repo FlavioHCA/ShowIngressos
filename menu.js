610b44e54235d60fbc8f30b5ac2cb7d294204466
FlávioHCA <flaviocastro0901@gmail.com>
2022-02-22 19:42:51 -0300

    Menu 0.3

--- menu.js
+++ menu.js
@@ -8,18 +8,21 @@ class MobileNavBar {
         this.handleClick = this.handleClick.bind(this);
       }
 
-    animateLinks() {
-        this.navLinks.forEach ((link) => {
-            link.style.animation
-            ? (link.style.animation = " ")
-            : (link.style.animation = '')
+      animateLinks() {
+        this.navLinks.forEach((link, index) => {
+          link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+                index / 7 + 0.3
+              }s`);
         });
-    }
-
-    handleClick() {
+      }
+    
+      handleClick() {
         this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
         this.animateLinks();
-    }
+      }
 
     addClickEvent() {
         this.mobileMenu.addEventListener("click", this.handleClick);
